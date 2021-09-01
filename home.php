a7f25b14d8de2e41c8274bad2ed06e64a193d87b
icaro2222 <icarodias2222@gmail.com>
2021-09-01 10:19:34 -0300

    de

--- home.php
+++ home.php
@@ -1,6 +1,5 @@
 <?php
-define("NOME", "icaro");
-echo NOME;
+
 ?>
 <!DOCTYPE html>
 <html lang="pt">
@@ -13,13 +12,13 @@ echo NOME;
 <body>
 	<div>
 		<div class="">
-			<form action="">
+			<form action="dados.php">
 				<hr>
 				<h1>Formulario</h1>
 				<p>vamos ver</p>
-				<input type="email" name="email" id="id_email"><br><br>
-				<input type="text" name="nome" id="id_nome"><br><br>
-				<input type="button" value="butao">
+				Nome: <input type="email" name="email" id="id_email"><br><br>
+				Email: <input type="text" name="nome" id="id_nome"><br><br>
+				<input type="submit" value="Enviar">
 				<img src="https://avatars.githubusercontent.com/u/71037296?s=48&v=4" alt="">
 				<a href="dados.php">Clique aqui</a>
 			</form>
